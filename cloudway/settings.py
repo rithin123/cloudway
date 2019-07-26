0c0526e5819c416be156a4a5cc3cb60534c35e06
rithin123 <ritn007@gmail.com>
2019-07-26 08:37:09 +0530

    cloud_test1

--- cloudway/settings.py
+++ cloudway/settings.py
@@ -117,4 +117,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/static/' #this is how we refer them
+STATICFILES_DIR= [
+    os.path.join(BASE_DIR,'static')
+]
+
+STATIC_ROOT= os.path.join(BASE_DIR,'assets')#django will put all the static files into assests folder
--- cloudway/urls.py
+++ cloudway/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('malaria.urls')),
+  #  path('', include('malaria.urls')),
+  path('', include('travello.urls')),
     path('admin/', admin.site.urls),
 ]
